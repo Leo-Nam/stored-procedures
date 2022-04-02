0ca6ff57e27682890c836e61fbd804f5ee0c5bb3
Leo Nam <myleobot@gmail.com>
2022-04-02 22:03:31 +0900

    20220401

--- sp_add_sigungu_without_handler.sql
+++ sp_add_sigungu_without_handler.sql
@@ -16,38 +16,44 @@ AUTHOR 			: Leo Nam
 Change			: 반환 타입은 레코드를 사용하기로 함. 모든 프로시저에 공통으로 적용(0.0.2)
 */		
 
-    SELECT COUNT(B_CODE) INTO @B_CODE_COUNT
-    FROM KIKCD_B
-    WHERE 
-		B_CODE = IN_SIGUNGU_CODE AND
-        CANCELED_DATE IS NULL;
-	IF @B_CODE_COUNT > 0 THEN
-		CALL sp_req_current_time(@REG_DT);
-		INSERT INTO BUSINESS_AREA (
-			SITE_ID, 
-			KIKCD_B_CODE, 
-			IS_DEFAULT, 
-			CREATED_AT
-		) 
-		VALUES (
-			@SITE_ID, 
-			IN_SIGUNGU_CODE, 
-			IN_IS_DEFAULT, 
-			@REG_DT
-		);
-		
-		IF ROW_COUNT() = 1 THEN
-		/*레코드가 정상적으로 생성되었다면*/
-			SET @rtn_val 		= 0;
-			SET @msg_txt 		= 'success';
+	IF IN_SIGUNGU_CODE IS NOT NULL THEN
+		SELECT COUNT(B_CODE) INTO @B_CODE_COUNT
+		FROM KIKCD_B
+		WHERE 
+			B_CODE = IN_SIGUNGU_CODE AND
+			CANCELED_DATE IS NULL;
+		IF @B_CODE_COUNT > 0 THEN
+			CALL sp_req_current_time(@REG_DT);
+			INSERT INTO BUSINESS_AREA (
+				SITE_ID, 
+				KIKCD_B_CODE, 
+				IS_DEFAULT, 
+				CREATED_AT
+			) 
+			VALUES (
+				@SITE_ID, 
+				IN_SIGUNGU_CODE, 
+				IN_IS_DEFAULT, 
+				@REG_DT
+			);
+			
+			IF ROW_COUNT() = 1 THEN
+			/*레코드가 정상적으로 생성되었다면*/
+				SET @rtn_val 		= 0;
+				SET @msg_txt 		= 'success';
+			ELSE
+			/*레코드가 정상적으로 생성되지 않았다면*/
+				SET @rtn_val 		= 35301;
+				SET @msg_txt 		= 'Failed to add area of ​​interest';
+			END IF;
 		ELSE
-		/*레코드가 정상적으로 생성되지 않았다면*/
-			SET @rtn_val 		= 35301;
-			SET @msg_txt 		= 'Failed to add area of ​​interest';
+		/*B_CODE가 존재하지 않는 경우에는 예외처리한다.*/
+			SET @rtn_val 		= 35302;
+			SET @msg_txt 		= 'B_CODE does not exist';
 		END IF;
     ELSE
 	/*B_CODE가 존재하지 않는 경우에는 예외처리한다.*/
-		SET @rtn_val 		= 35302;
-		SET @msg_txt 		= 'B_CODE does not exist';
+		SET @rtn_val 		= 35303;
+		SET @msg_txt 		= 'B_CODE should not be null';
     END IF;
 END
